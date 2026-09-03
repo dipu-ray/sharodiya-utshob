58cddf12038281dbd0ac6fb99fac9eb76c33d940
dipu-ray <dipu34786@gmail.com>
2026-09-04 03:02:59 +0600

    To add and developed scroll to top button

--- js/main.js
+++ js/main.js
@@ -168,3 +168,23 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }, 1000);
 });
+
+
+
+
+
+// Scroll to Top Button Logic's
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        scrollTopBtn.classList.add('show');
+    } else {
+        scrollTopBtn.classList.remove('show');
+    }
+});
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
